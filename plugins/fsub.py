65c77f1194598a20b78bb613d89cf4d5f127edb1
CODEDISE <otxstudioofficial@gmail.com>
2026-01-05 00:11:54 +0530

    Delete plugins/fsub.py

--- plugins/fsub.py
+++
@@ -1,3 +0,0 @@
-# +++ Modified By Yato [telegram username: @i_killed_my_clan & @ProYato] +++ # aNDI BANDI SANDI JISNE BHI CREDIT HATAYA USKI BANDI RAndi 
-
-# gand marao, public repo me na krha me add force sub 
